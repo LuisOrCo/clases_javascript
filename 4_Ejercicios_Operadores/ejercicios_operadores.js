7068d751e8e08e69a48101237beb9dc016eed5a4
LuisOrCo <angelortegacorrea09@gmail.com>
2026-05-24 08:48:33 -0500

    Hice los ejercicios de ejercicios_operadores

--- 4_Ejercicios_Operadores/ejercicios_operadores.js
+++ 4_Ejercicios_Operadores/ejercicios_operadores.js
@@ -11,6 +11,15 @@
  */
 // Tu código aquí:
 
+let puntaje = 10;
+
+puntaje+=5
+puntaje*=3
+puntaje-=10
+puntaje/=2
+
+console.log(puntaje)
+
 
 /**
  * EJERCICIO 2: El Enigma de la Igualdad
@@ -23,6 +32,12 @@
  */
 // Respuestas:
 
+console.log(10 == "10") // esto da true porque no verifica el tipo de dato
+console.log(10 === "10") // esto da false porque verifica también el tipo de dato
+console.log(0 == false) // esto da true porque verifica el 0 por conversión implícita como booleano
+console.log(0 === false) // esto da false porque ahora la comparación es estricta y verifica los tipos de datos, donde 0 es number y false es booleano
+
+
 
 /**
  * EJERCICIO 3: Lógica de "Cortocircuito"
@@ -34,6 +49,16 @@
  */
 // Respuestas:
 
+let resultado1 = "Hola" || "Mundo"
+console.log(resultado1)
+
+let resultado2 = 0 && "JavaScript"
+console.log(resultado2)
+
+let resultado3 = null ?? "Valor por defecto"
+console.log(resultado3)
+
+
 
 /**
  * EJERCICIO 4: Clasificador con Operador Ternario
@@ -43,6 +68,9 @@
  */
 // Tu código aquí:
 
+let temperatura = 31
+let clima = status = (temperatura > 30) ? "Calor": "Fresco";
+console.log(clima)
 
 /**
  * EJERCICIO 5: Identificando Tipos (Operador Unario)
@@ -56,6 +84,13 @@
  */
 // Respuestas:
 
+console.log(typeof(42))
+console.log(typeof("Aprendiz"))
+console.log(typeof(true))
+console.log(typeof({nombre: "JS"}))
+console.log(typeof(undefined))
+console.log(typeof(42n))
+
 
 /**
  * EJERCICIO 6: Verificación de Propiedades (Relacional 'in')
@@ -66,6 +101,10 @@
  */
 // Tu código aquí:
 
+const laptop = { marca: "Dell", ram: 16 };
+console.log("ram" in laptop)
+console.log("almacenamiento" in laptop)
+
 
 /**
  * EJERCICIO 7: Trabajando con Grandes Números (BigInt)
@@ -75,6 +114,14 @@
  */
 // Corrección:
 
+/**
+ * Esto da error porquue no se pueden mezclar tipos de datos numbers y bigint para no perder precisión en operaciones
+ *  que involucren bigints
+ * /
+
+const total = 100n + BigInt(50)
+console.log(total)
+
 
 /**
  * EJERCICIO 8: El Bucle de la Coma
@@ -83,6 +130,14 @@
  * Explica qué función cumple la coma en la inicialización y actualización.
  */
 // Explicación:
+
+for (let i = 0, j = 10; i <= 5; i++, j--) { console.log(i, j); }
+
+/**
+ * la coma hace que se pueda poner el segundo iterador el cual es el del segundo ciclo, haciendo un segundo ciclo a la vez que el 
+ * primero y todo en la misma línea 
+ * */
+
 
 
 /**
@@ -94,6 +149,16 @@
  */
 // Respuestas:
 
+let x = 5; let y = 5;
+let a = ++x;
+let b = y++;
+
+console.log(x,y,a,b)
+/**
+ * Aquí el resultado es 6,6,6,5, esto porque el ++x hace que primero se incremente el valor de x antes de almacenarse en a
+ * en cambio el y++ hace que b primero reciba el valor de y que es 5 y luego se incremente, por lo que y queda 6 pero b 5
+ * */
+
 
 /**
  * EJERCICIO 10: Validación de Instancias (Relacional 'instanceof')
@@ -104,3 +169,7 @@
  */
 // Respuestas:
 ```
+
+console.log(new Date() instanceof Date);
+console.log([] instanceof Array);
+console.log([] instanceof Object);
