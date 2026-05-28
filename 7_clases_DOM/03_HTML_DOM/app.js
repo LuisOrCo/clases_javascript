673837bebf0211f9f3bd3391f671b1b4e61c316f
LuisOrCo <angelortegacorrea09@gmail.com>
2026-05-28 10:35:58 -0500

    Nuevos cambios de los ejercicios de DOM

--- 7_clases_DOM/03_HTML_DOM/app.js
+++ 7_clases_DOM/03_HTML_DOM/app.js
@@ -2,7 +2,19 @@
 const mensaje = document.getElementById("mensaje");
 const boton = document.getElementById("cambiar");
 
+let activado = false
+
 boton.addEventListener("click", () => {
-    document.body.style.backgroundColor = "lightblue";
-    mensaje.textContent = "El HTML DOM permite cambiar estilos y contenido en tiempo real.";
-});
+
+    if (!activado){
+        document.body.style.backgroundColor = "lightblue";
+        mensaje.textContent = "El HTML DOM permite cambiar estilos y contenido en tiempo real.";
+        activado = true
+    }else{
+        document.body.style.backgroundColor = "white";
+        mensaje.textContent = "";
+
+        activado = false;
+    }
+    });
+    
